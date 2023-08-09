326c0a261dbe8852b8a7ff77f156940887048e72
Mohsin/918 <mohsinquantum@gmail.com>
2023-08-09 09:57:34 +0530

    initial commit

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="p-5 bg-primary flex items-center justify-center">
+      <h1 className="text-secondary text-4xl font-semibold mr-4">Mohsin</h1>
+      <h1 className="text-white text-4xl font-semibold">Ahmad</h1>
+    </div>
+  );
+}
+
+export default Header;
